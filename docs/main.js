3fe71a16600f7f9143628150ed4eae6e2e50f191
Nate Wildermuth <nwildermuth@nowthismedia.com>
2015-01-28 10:54:02 -0600

    Added filtering by category, either by input field, or by clicking on label.

--- docs/main.js
+++ docs/main.js
@@ -1,57 +1,68 @@
 /* global document */
 
 function toArray(xs) {
-    return Array.prototype.slice.call(xs);
+  return Array.prototype.slice.call(xs);
+}
+
+function filterTocType(category) {
+  nameFilter.value = category;
+  filterToc();
 }
 
 function filterToc() {
-    var f = filterElement.bind(null, nameFilter.value, '');
-    funcs.forEach(f);
+  var f = filterElement.bind(null, nameFilter.value, nameFilter.value);
+  funcs.forEach(f);
 }
 
 function filterElement(nameFilter, categoryFilter, elem) {
-    var name = elem.getAttribute('data-name');
-    var category = elem.getAttribute('data-category');
-    var matches = strIn(nameFilter, name) || category === categoryFilter;
-    elem.style.display = matches ? '' : 'none';
+  var name = elem.getAttribute('data-name');
+  var category = elem.getAttribute('data-category');
+  var matches = strIn(nameFilter, name) || category === categoryFilter;
+  elem.style.display = matches ? '' : 'none';
 }
 
 function strIn(a, b) {
-    a = a.toLowerCase();
-    b = b.toLowerCase();
-    return b.indexOf(a) >= 0;
+  a = a.toLowerCase();
+  b = b.toLowerCase();
+  return b.indexOf(a) >= 0;
 }
 
 function scrollToTop() {
-    var main = document.querySelector('main');
-    main.scrollTop = 0;
+  var main = document.querySelector('main');
+  main.scrollTop = 0;
 }
 
 function tryToggleCollapse(elem) {
-    var sel = elem && elem.getAttribute('data-collapser');
-    if (!sel) { return; }
-    elem
-        .classList
-        .toggle('open');
-    document
-        .querySelector(sel)
-        .classList
-        .toggle('in');
+  var sel = elem && elem.getAttribute('data-collapser');
+  if (!sel) {
+    return;
+  }
+  elem
+    .classList
+    .toggle('open');
+  document
+    .querySelector(sel)
+    .classList
+    .toggle('in');
 }
 
 function isTopLink(elem) {
-    return elem.getAttribute('href') === '#';
+  return elem.getAttribute('href') === '#';
 }
 
 function dispatchEvent(event) {
-    var target = event.target;
-    var parent = target.parentNode;
-    if (isTopLink(target)) {
-        scrollToTop(target);
-    } else {
-        tryToggleCollapse(target);
-        tryToggleCollapse(parent);
-    }
+  var target = event.target,
+    parent = target.parentNode,
+    category = target.getAttribute('data-category');
+  if (category) {
+    filterTocType(category)
+  }
+  if (isTopLink(target)) {
+    scrollToTop(target);
+  } else {
+    tryToggleCollapse(target);
+    tryToggleCollapse(parent);
+  }
 }
 
 var nameFilter = document.getElementById('name-filter');
